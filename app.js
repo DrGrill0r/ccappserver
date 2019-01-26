5316b6f05240d08e32f450a5d842919fb0231f26
DrGrill0r <DrGrill0r@users.noreply.github.com>
2019-01-26 02:22:56 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var data = new Array;
+
+app.use(cors());
+app.use(bodyParser.json({ type: 'application/json' }));
+
+function Category(id, name) {
+  this.id = id;
+  this.name = name;
+}
+
+var Categories = new Array;
+
+app.get("/v1/categories", (req, res, next) => {
+  res.json(Categories);
+  res.statusCode = 200;
+});
+
+app.post("/v1/categories", (req, res) => {
+  console.log(req.body);
+  Categories = req.body;
+  req.statusCode = 200;
+});
+
+app.delete("/v1/categories", (req, res) => {
+  console.log(req.body);
+  req.statusCode = 200;
+});
+
+app.listen(80, () => {
+ console.log("Server running on port 3000");
+});
